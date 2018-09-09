2b80eff03d8db8059c571cbcfe26f57c57bc265a
Thomas Gordon <nrgetik@users.noreply.github.com>
2018-09-09 19:40:37 -0400

    Characters per line, parameter descriptions

--- slack-creep.py
+++ slack-creep.py
@@ -11,15 +11,14 @@ from time import sleep, time
 USERS = ['slackbot']
 
 @click.command()
-@click.option("-t", "--token", is_flag=False, type=click.STRING,
-              metavar="<token>", required=True, help="Slack API Token")
-@click.option("-u", "--users", is_flag=False, default=','.join(USERS),
-              show_default=True, type=click.STRING, metavar="<users>",
+@click.option("-t", "--token", is_flag=False, type=click.STRING, metavar="<token>", required=True,
+              help="Slack API Token")
+@click.option("-u", "--users", is_flag=False, default=','.join(USERS), show_default=True,
+              type=click.STRING, metavar="<users>",
               help="Single or comma-separated list of users to alert on")
-@click.option("-a", "--age", is_flag=False, default=60, show_default=True,
-              type=click.INT, metavar="<age>",
-              help="Maximum age, in seconds, of results we care about. " \
-              "Will also determine the sleep value between search clusters")
+@click.option("-a", "--age", is_flag=False, default=60, show_default=True, type=click.INT,
+              metavar="<age>", help="Maximum age, in seconds, of messages that are relevant. " \
+              "Will also determine the sleep value between groups of searches")
 def main(token, users, age):
     users = [u.strip() for u in users.split(",")]
     try:
@@ -29,8 +28,7 @@ def main(token, users, age):
                            "query": "from:@{}".format(user),
                            "count": 1,
                            "sort": "timestamp"}
-                r = requests.get("https://slack.com/api/search.messages",
-                                 params=payload)
+                r = requests.get("https://slack.com/api/search.messages", params=payload)
                 if r.status_code != requests.codes.ok:
                     r.raise_for_status()
                 else:
